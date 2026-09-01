e087fc5bca28fc349c1ac4a10d264f9f14586343
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-09-01 13:00:21 +0700

    Add keycloal user catalog

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -25,17 +25,18 @@ backend.add(import('@backstage/plugin-techdocs-backend'));
 
 // auth plugin
 backend.add(import('@backstage/plugin-auth-backend'));
+// See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
 backend.add(
   import('@backstage-community/plugin-auth-backend-module-keycloak-provider'),
 );
-// See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
-backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
-// See https://backstage.io/docs/auth/guest/provider
 
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend'));
 backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
+);
+backend.add(
+  import('@backstage-community/plugin-catalog-backend-module-keycloak'),
 );
 
 // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
