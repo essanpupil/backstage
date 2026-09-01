400032667b6b62be14b82c454b77a84e81b282cb
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-09-01 14:13:08 +0700

    Configure backstage singin frontend with lkeycloak

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,8 +1,86 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import { navModule } from './modules/nav';
-import { homeModule } from './modules/home';
 
+import {
+  OpenIdConnectApi,
+  ProfileInfoApi,
+  BackstageIdentityApi,
+  SessionApi
+} from '@backstage/core-plugin-api';
+import { SignInPageBlueprint } from '@backstage/plugin-app-react';
+import { SignInPage } from '@backstage/core-components';
+import { 
+  ApiBlueprint,
+  createApiRef,
+  discoveryApiRef,
+  oauthRequestApiRef,
+  configApiRef,
+  createFrontendModule 
+} from '@backstage/frontend-plugin-api';
+import { OAuth2 } from '@backstage/core-app-api';
+
+
+// Define the API Reference
+const keycloakAuthApiRef = createApiRef<
+  OpenIdConnectApi & ProfileInfoApi & BackstageIdentityApi & SessionApi
+>({
+  id: 'auth.keycloak',
+});
+
+// Create the API Implementation Blueprint
+const keycloakAuthApi = ApiBlueprint.make({
+  name: 'keycloak',
+  params: defineParams =>
+    defineParams({
+      api: keycloakAuthApiRef,
+      deps: {
+        discoveryApi: discoveryApiRef,
+        oauthRequestApi: oauthRequestApiRef,
+        configApi: configApiRef,
+      },
+      factory: ({ discoveryApi, oauthRequestApi, configApi }) =>
+        OAuth2.create({
+          configApi,
+          discoveryApi,
+          oauthRequestApi,
+          environment: configApi.getOptionalString('auth.environment'),
+          provider: {
+            id: 'keycloak',
+            title: 'Keycloak',
+            icon: () => null,
+          },
+          defaultScopes: ['openid', 'profile', 'email'],
+        }),
+    }),
+});
+
+// Configure the Sign-In Page
+const signInPage = SignInPageBlueprint.make({
+  params: {
+    loader: async () => props =>
+      (
+        <SignInPage
+          {...props}
+          provider={{
+            id: 'keycloak-auth-provider',
+            title: 'Keycloak',
+            message: 'Sign in using Keycloak',
+            apiRef: keycloakAuthApiRef,
+          }}
+        />
+      ),
+  },
+});
+
+// Register in the App
 export default createApp({
-  features: [catalogPlugin, navModule, homeModule],
+  features: [
+    catalogPlugin,
+    navModule,
+    createFrontendModule({
+      pluginId: 'app',
+      extensions: [keycloakAuthApi, signInPage],
+    }),
+  ],
 });
